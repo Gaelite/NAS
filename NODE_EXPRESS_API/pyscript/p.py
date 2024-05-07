c7dd89415b199ecbc4e75e2ea433e13981a789f6
Gael García <129134162+Gaelite@users.noreply.github.com>
2024-05-07 13:20:37 +0000

    Archivos organizados

---
+++ NODE_EXPRESS_API/pyscript/p.py
@@ -0,0 +1,22 @@
+from netmiko import ConnectHandler
+import textfsm
+from functions import get_device_neighbor_details, get_device_info
+device = {
+        'device_type': 'cisco_ios', 
+        'host': '192.168.1.1',
+        'username': 'gmedina',
+        'password': 'cisco',
+    }
+
+cdp_template = textfsm.TextFSM(open("Templates/show_cdp_neighbor_detail.textfsm"))
+try:
+    ssh_session = ConnectHandler(**device)
+    cdp_result= ssh_session.find_prompt() + "\n"#Te da el modo actual de CLI junto con el nombre del host
+
+    cdp_result += ssh_session.send_command("show cdp neighbor detail", delay_factor=2)#te consigue la informacion CD
+    #procesar info para enviar a servidor
+    fsm_cdp_results = cdp_template.ParseText(cdp_result)
+    print(fsm_cdp_results)
+    ssh_session.disconnect()
+except Exception as e:
+    print("Error al iniciar sesión SSH:", str(e))
