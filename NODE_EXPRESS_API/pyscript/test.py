024394d3f920f611bb7bea3441721b5fbbff1f10
Gael García <129134162+Gaelite@users.noreply.github.com>
2024-05-13 23:22:01 -0600

    Descubrimiento de nat

--- NODE_EXPRESS_API/pyscript/test.py
+++ NODE_EXPRESS_API/pyscript/test.py
@@ -1,3 +1,21 @@
-import sys
+from netmiko import ConnectHandler
+import textfsm
+from functions import get_device_neighbor_details, get_device_info
+device = {
+        'device_type': 'cisco_ios', 
+        'host': '192.168.3.2',
+        'username': 'gmedina',
+        'password': 'cisco',
+        'secret': 'cisco'
+    }
 
-print(sys.argv[1])
+cdp_template = textfsm.TextFSM(open("Templates/show_ip_nat_statistics.textfsm"))
+
+ssh_session = ConnectHandler(**device)
+cdp_result= ssh_session.find_prompt() + "\n"#Te da el modo actual de CLI junto con el nombre del host
+
+cdp_result += ssh_session.send_command("show ip nat statistics", delay_factor=2)#te consigue la informacion CD
+
+res = cdp_template.ParseText(cdp_result)
+
+print(res)
